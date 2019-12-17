a93c08706aa9ebc65ac728ffc6ab43ba3173c030
r.pilipenko <r.pilipenko@smileexpo.com.ua>
2019-12-17 18:01:07 +0200

    show news list add create new action

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
@@ -17,10 +19,36 @@ class NewsController extends AbstractController
      */
     public function index(Request $request)
     {
-        $news = [1, 2, 3];
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+        $news = $repository->findAll();
 
         return $this->render('/news/list.html.twig', [
             'news' => $news
         ]);
+    }
+
+
+    /**
+     * @return Response
+     * @Route("/news/create", name="create_article")
+     */
+    public function createNew():Response
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $new = new Article();
+        $new->setCreatedAt(new \DateTime());
+        $new->setImage('/news/default.jpg');
+        $new->setStatus(1);
+        $new->setText("This is a first article in symfony project");
+        $new->setTitle("First new");
+        $new->setUpdatedAt(new \DateTime());
+
+        $entityManager->persist($new);
+
+        //$entityManager->flush();
+
+        return new Response("Saved new product with id {$new->getId()}");
     }
 }
