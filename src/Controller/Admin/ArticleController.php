7e424af9e1e3285f146ba8a16c3053f15dc87d33
r.pilipenko <r.pilipenko@smileexpo.com.ua>
2019-12-19 13:43:56 +0200

    forms and flashes

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -3,8 +3,14 @@
 
 namespace App\Controller\Admin;
 
-
+use App\Entity\Article;
+use App\Form\Type\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -15,5 +21,39 @@ class ArticleController extends AbstractController
     public function index()
     {
         return $this->render('/admin/articles/list.html.twig');
+    }
+
+
+    /**
+     * @Route("/admin/articles/create", name="admin_create_article")
+     */
+    public function create(Request $request)
+    {
+        $article = new Article();
+        $article->setStatus(1);
+        $article->setImage('/news/default.jpg');
+        $article->setUpdatedAt(new \DateTime());
+        $article->setCreatedAt(new \DateTime());
+        $article->setTitle("Write a title...");
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', "Your changes were saved!");
+
+            return $this->redirectToRoute('AdminArticles');
+        }
+
+        return $this->render('/admin/articles/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
