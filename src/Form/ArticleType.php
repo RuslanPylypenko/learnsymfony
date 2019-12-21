147dba1f94fc98e451968858520e3cb351da023f
ruslan <ruslan.php.dev@gmail.com>
2019-12-21 10:20:43 +0200

    add new entity crud

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('image')
+            ->add('text')
+            ->add('status')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
